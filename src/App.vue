6a62b5cb0b1b63914a503e2c1c535877621a08be
caicailv <1401394657@qq.com>
2022-01-12 18:12:41 +0800

    'c'

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,46 @@
 
 <template>
 	<div>
-		app
-		<mycm></mycm>
-
+		<div id="example">
+			<p>{{ name }}</p>
+			<button @click="changeName">change</button>
+			<button @click="changeName2">change last</button>
+		</div>
 	</div>
 </template>
 <script>
 export default {
 	data() {
 		return {
-      aaa:'啊哈啊'
-    };
+			firstName: '黄',
+			lastName: '一',
+			useless: 0,
+		};
 	},
-
-	created(){
-
-	}
+	methods: {
+		changeName() {
+			this.useless++;
+			this.firstName = '张';
+		},
+		changeName2() {
+			this.lastName = '二';
+		},
+	},
+	computed: {
+		name() {
+			if (this.useless > 0) {
+				return this.firstName + this.lastName;
+			}
+			return 'please click change';
+		},
+	},
+	created() {
+		// let i = 0;
+		// setInterval(() => {
+		// 	this.fullName = this.fullName + i;
+		// 	i++
+		// }, 1000);
+		this.$forceUpdate()
+	},
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,42 +1,7 @@
-const Svl = function (option) {
-  let { el, data } = option
-  el = document.querySelector(el)
-  // 匹配数据绑定
-  let html = el.outerHTML
-  let reg = /\{\{.\}\}/g
-}
+import Vue from 'vue'
+import App from './App.vue'
 
-new Svl({
+new Vue({
   el: '#app',
-  data: {
-    msg: 'mysvl',
-  },
-})
-
-var pendingText = '姓名：{{user.name}}，年龄：{{user.age}}'
-function parseText(pendingText) {
-  var collection = []
-  var searchTxt = ''
-  var targetTxt = ''
-  var stringLength = 0,
-    lastIndex = 0,
-    curIndex = 0
-  var reg = /\{\{(.+?)\}\}/g
-
-  if (!reg.test(pendingText)) {
-    throw new Error('未匹配')
-  } else {
-    reg.lastIndex = 0
-    while ((tempR = reg.exec(pendingText))) {
-      curIndex = reg.lastIndex
-      searchTxt = tempR[0]
-      stringLength = searchTxt.length
-      collection.push(pendingText.slice(lastIndex, curIndex - stringLength))
-      targetTxt = tempR[1]
-      collection.push(targetTxt)
-      lastIndex = curIndex
-    }
-  }
-  console.log(collection)
-}
-parseText(pendingText)
+  render: (h) => h(App),
+}).$mount()
