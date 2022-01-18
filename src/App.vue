2919febe716d939df38f0f795d177540296e2ff1
caicailv <1401394657@qq.com>
2022-01-18 18:03:14 +0800

    'c'

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,31 @@
 
 <template>
 	<div>
-		<div id="example">
-			<p>{{ name }}</p>
-			<button @click="changeName">change</button>
-			<button @click="changeName2">change last</button>
-		</div>
+		<p>
+			{{ msg.a }}
+			{{msg}}
+		</p>
+		<button @click="change">click</button>
 	</div>
 </template>
 <script>
 export default {
 	data() {
 		return {
-			firstName: '黄',
-			lastName: '一',
-			useless: 0,
+			msg: {
+				a: 2,
+			},
 		};
 	},
+
+	computed: {},
 	methods: {
-		changeName() {
-			this.useless++;
-			this.firstName = '张';
-		},
-		changeName2() {
-			this.lastName = '二';
+		change() {
+			this.msg.a = 4;
+			this.msg.b = 2
+			
 		},
 	},
-	watch:{
-		firstName(){
-			console.log('firstName');
-		},
-		useless:[
-			()=>{
-				console.log('a');
-			},
-			()=>{
-				console.log('b');
-			}
-		]
-	},
-	computed: {
-		name() {
-			if (this.useless > 0) {
-				return this.firstName + this.lastName;
-			}
-			return 'please click change';
-		},
-	},
-	created() {
-		// let i = 0;
-		// setInterval(() => {
-		// 	this.fullName = this.fullName + i;
-		// 	i++
-		// }, 1000);
-		this.$forceUpdate()
-	},
+	created() {},
 };
 </script>
